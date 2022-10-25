aa11d0e9a2a22233164c284bd638abc2131deb29
Aariyan Apu <aariyan.apu@gmail.com>
2022-10-26 01:37:16 +0600

    First Commit Added Component

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SimpleCentered from "./SimpleCentered";
+import SplitWithNavbar from "./SplitWithNavbar";
+
+const HeroSection = () => {
+  return (
+    <>
+      <SimpleCentered />
+      <SplitWithNavbar/>
+    </>
+  );
+};
+
+export default HeroSection;
